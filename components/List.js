18140d8e6d74b8145339947173d754602729c921
Sam-2019 <samtmartey@gmail.com>
2022-11-05 21:42:32 +0000

    added list

---
+++ components/List.js
@@ -0,0 +1,39 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+
+export default function List({data}) {
+  if (!data) {
+    return (
+      <View style={styles.view}>
+        <Text>No Data</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.view}>
+      <View>
+        <Text>{data.name}</Text>
+        <Text>{data.other_name}</Text>
+        <Text>{data.email}</Text>
+        <Text>{data.gender}</Text>
+        <Text>{data.image}</Text>
+        <Text>{data.countryCode}</Text>
+        <Text>{data.e164Format}</Text>
+        <Text>{data.numberType}</Text>
+        <Text>{data.bank_id}</Text>
+        <Image source={{uri: data.image}} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    margin: 10,
+  },
+  image: {
+    width: 400,
+    height: 400,
+  },
+});

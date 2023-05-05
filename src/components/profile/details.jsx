4590b612fc046ac898c22502e5f7f05c99a9139a
Samuel Martey <sam@encodevlabs.com>
2023-05-05 12:37:37 +0000

    removed ununsed imports

--- src/components/profile/details.jsx
+++ src/components/profile/details.jsx
@@ -1,11 +1,7 @@
-import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
 import {
- EllipsisVerticalIcon,
  PhoneIcon,
  EnvelopeIcon,
 } from "@heroicons/react/20/solid";
-import { people, classNames } from "../../utils";
 
 // eslint-disable-next-line react/prop-types
 function Details({ data }) {
@@ -50,55 +46,6 @@ function Details({ data }) {
         <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
         Email
        </a>
-
-       <div className="ml-3 inline-flex sm:ml-0">
-        {/* <Menu as="div" className="relative inline-block text-left">
-         <Menu.Button className="inline-flex items-center rounded-md bg-white p-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          <span className="sr-only">Open options menu</span>
-          <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
-         </Menu.Button>
-         <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-         >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-           <div className="py-1">
-            <Menu.Item>
-             {({ active }) => (
-              <a
-               href="#"
-               className={classNames(
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                "block px-4 py-2 text-sm"
-               )}
-              >
-               View profile
-              </a>
-             )}
-            </Menu.Item>
-            <Menu.Item>
-             {({ active }) => (
-              <a
-               href="#"
-               className={classNames(
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                "block px-4 py-2 text-sm"
-               )}
-              >
-               Copy profile link
-              </a>
-             )}
-            </Menu.Item>
-           </div>
-          </Menu.Items>
-         </Transition>
-        </Menu> */}
-       </div>
       </div>
      </div>
     </div>

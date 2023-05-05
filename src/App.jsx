71af293dc51a383ad07d266341f98773589721ed
Samuel Martey <sam@encodevlabs.com>
2023-05-05 14:19:19 +0000

    imported banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Identity from "./identity";
 import "./App.css";
+import Banner from "./components/banner";
 
 function App() {
- return <Identity />;
+ return (
+  <>
+   <Banner />
+   <Identity />
+  </>
+ );
 }
 
 export default App;

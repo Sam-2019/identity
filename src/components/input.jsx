1b50978b45923ac646c4f4672d0ea5b36d88b085
Samuel Martey <sam@encodevlabs.com>
2023-05-05 09:43:24 +0000

    input update

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,16 +1,17 @@
 // eslint-disable-next-line react/prop-types
 function Input({ input, setInput, handleSearch, loading }) {
  return (
-  <div className="mt-2 flex rounded-md">
+  <div className="flex rounded-md">
    <div className="relative flex flex-grow items-stretch focus-within:z-10">
     <input
+     required
      value={input}
      onChange={(e) => setInput(e.target.value)}
      type="number"
      name="name"
      id="name"
      className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-     placeholder="Jane Smith"
+     placeholder="0244444444"
     />
    </div>
    <button

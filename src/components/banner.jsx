a513fcb1c11f22fdc4159bc312db2a2d7b6f1960
Samuel Martey <sam@encodevlabs.com>
2023-05-05 14:19:03 +0000

    added banner

---
+++ src/components/banner.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Banner() {
+ return (
+  <div className="relative flex justify-center gap-x-6 overflow-hidden bg-gray-900 px-6 py-2.5 sm:px-3.5">
+ 
+
+   <div className="gap-x-4 gap-y-2">
+    <p className="text-sm leading-6 text-white">
+     <strong className="font-semibold">
+      <span role="img" aria-label="ghana_flag">
+       🇬🇭
+      </span>
+     </strong>
+     <span className="text-sm ml-2">ONLY</span>
+    </p>
+   </div>
+  
+  </div>
+ );
+}

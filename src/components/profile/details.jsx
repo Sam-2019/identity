2b59cb2ec93120d399ae3ba09001616369d9c4c2
Samuel Martey <sam@encodevlabs.com>
2023-05-17 17:42:13 +0000

    fixed mailto

--- src/components/profile/details.jsx
+++ src/components/profile/details.jsx
@@ -35,7 +35,7 @@ function Details({ details }) {
        </a>
 
        <a
-        href={details.email}
+        href={`mailto: ${details.email}`}
         className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 border border-transparent text-sm font-semibold text-gray-900
             inline-flex w-full rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50
             "

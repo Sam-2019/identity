214117011720b051145767b0bf728792e3f24987
Sam-2019 <samtmartey@gmail.com>
2024-01-05 18:32:43 +0000

    removed whitespace

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,8 +3,6 @@
 export default function Banner() {
  return (
   <div className="relative flex justify-center gap-x-6 overflow-hidden bg-orange-600 px-6 py-2.5 sm:px-3.5">
- 
-
    <div className="gap-x-4 gap-y-2">
     <p className="text-sm leading-6 text-white">
      <strong className="font-semibold">

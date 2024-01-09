35a98709665c9ed6e5e3c064e3673b985b23c2ac
Sam-2019 <samtmartey@gmail.com>
2024-01-09 12:30:07 +0000

    Fix variable name in Profile component

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,24 +1,24 @@
 import PropTypes from "prop-types";
 import { Details } from "./compositions";
 
-function Profile({ detail }) {
+function Profile({ details }) {
   return (
     <Details>
-      <Details.Image>{detail.image}</Details.Image>
+      <Details.Image>{details.image}</Details.Image>
       <div className="mt-6 px-4 sm:mt-8 sm:flex sm:items-end sm:px-6">
         <div className="sm:flex-1">
           <div>
-            <Details.Name>{detail.name}</Details.Name>
-            <Details.OtherName>{detail.other_name}</Details.OtherName>
+            <Details.Name>{details.name}</Details.Name>
+            <Details.OtherName>{details.other_name}</Details.OtherName>
           </div>
           <div className="-mt-px flex divide-x divide-gray-200"></div>
           <div className="mt-5 flex flex-wrap space-x-2 sm:space-x-3 sm:space-y-0">
-            <Details.Phone>{detail.phone}</Details.Phone>
-            <Details.Email>{detail.email}</Details.Email>
+            <Details.Phone>{details.phone}</Details.Phone>
+            <Details.Email>{details.email}</Details.Email>
           </div>
         </div>
       </div>
-      <Details.Country>{detail.country}</Details.Country>
+      <Details.Country>{details.country}</Details.Country>
     </Details>
   );
 }
@@ -26,7 +26,7 @@ function Profile({ detail }) {
 export default Profile;
 
 Profile.propTypes = {
-  detail: PropTypes.object,
+  details: PropTypes.object,
   optionalObjectWithShape: PropTypes.shape({
     image: PropTypes.string,
     name: PropTypes.string,

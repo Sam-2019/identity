0da122aea892331f09957608485b804a68e5f7bb
Sam-2019 <samtmartey@gmail.com>
2024-03-12 12:10:23 +0000

    Refactor banner component

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,17 +1,10 @@
 export default function Banner() {
- return (
-  <div className="relative flex justify-center gap-x-6 overflow-hidden bg-orange-600 px-6 py-2.5 sm:px-3.5">
-   <div className="gap-x-4 gap-y-2">
-    <p className="text-sm leading-6 text-white">
-     <strong className="font-semibold">
-      <span role="img" aria-label="ghana_flag">
-       🇬🇭
-      </span>
-     </strong>
-     <span className="text-sm ml-2 text-black">ONLY</span>
-    </p>
-   </div>
-  
-  </div>
- );
+  return (
+    <div className="flex justify-center py-3.5 bg-orange-600">
+      <p>
+        <span role="img" aria-label="ghana_flag">🇬🇭</span>
+        <span className="text-sm ml-2 text-black">ONLY</span>
+      </p>
+    </div>
+  );
 }

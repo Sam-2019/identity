79eb5375f383781fa4afaf8804727b6f86605a42
Sam-2019 <samtmartey@gmail.com>
2024-03-12 14:06:08 +0000

    Update banner component styling

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className="flex justify-center bg-orange-600">
-      <p className="py-4">
+      <p className="py-3">
         <span role="img" aria-label="ghana_flag">🇬🇭</span>
         <span className="text-sm ml-2 text-black">ONLY</span>
       </p>

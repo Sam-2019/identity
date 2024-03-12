b8588f4438b10196ce0fe8b398a3448c682c7e7d
Sam-2019 <samtmartey@gmail.com>
2024-03-12 14:04:02 +0000

    Refactor banner component styling

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
-    <div className="flex justify-center py-3.5 bg-orange-600">
-      <p>
+    <div className="flex justify-center bg-orange-600">
+      <p className="py-4">
         <span role="img" aria-label="ghana_flag">🇬🇭</span>
         <span className="text-sm ml-2 text-black">ONLY</span>
       </p>

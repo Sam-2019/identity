9bf837f0ad1829a9b8f49b88dc0d0f9078a534a3
Sam-2019 <samtmartey@gmail.com>
2024-03-13 11:39:22 +0000

    Refactor search component styling

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 function Search({ input, setInput, handleSearch, status }) {
   return (
-    <div className="flex rounded-md">
-      <div className="relative flex flex-grow items-stretch focus-within:z-10">
+    <div className="flex gap-x-2">
+      <div className="w-9/12 items-stretch focus-within:z-10">
         <input
           required
           value={input}
@@ -11,7 +11,7 @@ function Search({ input, setInput, handleSearch, status }) {
           type="number"
           name="name"
           id="name"
-          className="block w-full rounded-full px-4 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 border-2 border-slate-300 hover:border-slate-400 focus:border-slate-400 active:border-slate-400 focus:outline-orange-300"
+          className="w-full rounded-full px-4 py-2 text-gray-900 shadow-sm placeholder:text-gray-400 border-2 border-slate-300 hover:border-slate-400 focus:border-slate-400 active:border-slate-400 focus:outline-orange-600"
           placeholder="0244444444"
         />
       </div>
@@ -19,7 +19,7 @@ function Search({ input, setInput, handleSearch, status }) {
         type="button"
         onClick={() => handleSearch()}
         disabled={status === "pending"}
-        className="ml-2 px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 rounded-full"
+        className="w-3/12 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 rounded-full"
       >
         {status === "pending" ? "Loading..." : "Search"}
       </button>

efcac5544fe57cd9f7b3efbf05f08f724d4cb143
Sam-2019 <samtmartey@gmail.com>
2024-03-13 12:42:57 +0000

    Add PlayAnnounce component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Fragment } from "react";
 import Identity from "./identity";
 import Banner from "./components/banner";
+import PlayAnnounce from "./components/play_announce";
 
 function App() {
- return (
-  <Fragment>
-   <Banner />
-   <Identity />
-  </Fragment>
- );
+  return (
+    <Fragment>
+      <Banner />
+      <Identity />
+      <PlayAnnounce />
+    </Fragment>
+  );
 }
 
 export default App;
